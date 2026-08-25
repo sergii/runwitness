2aef016eb77409143c027e52284eebdb3ddff0a0
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-08-25 14:28:46 +0300

    Route MCP command to read adapter

--- cmd/runwitness/main.go
+++ cmd/runwitness/main.go
@@ -4,8 +4,13 @@ import (
 	"os"
 
 	"github.com/sergii/runwitness/internal/gateintegration"
+	"github.com/sergii/runwitness/internal/mcpintegration"
 )
 
 func main() {
-	os.Exit(gateintegration.Main(os.Args[1:]))
+	args := os.Args[1:]
+	if len(args) == 1 && args[0] == "mcp" {
+		os.Exit(mcpintegration.Main())
+	}
+	os.Exit(gateintegration.Main(args))
 }
